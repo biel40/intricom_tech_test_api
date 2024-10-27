3724031ed7bcd3cb47c826f3035bc860c82380b7
Gabriel Borrás Serra <biel40@MacBook-Air-de-Gabriel.local>
2024-10-27 17:38:30 +0100

    sucessfully formatted ClientController

--- src/modules/controllers/client.controller.ts
+++ src/modules/controllers/client.controller.ts
@@ -7,50 +7,50 @@ import { CreateClientRequestDto } from '../models/dto/create_client_request.dto'
 @Controller('clients')
 export class ClientController {
 
-    constructor(
-        private readonly _clientService: ClientService
-    ) {
+  constructor(
+    private readonly _clientService: ClientService
+  ) {
 
-    }
+  }
 
-    @Get()
-    @ApiOperation({ summary: 'Listar todos los registros de clientes' })
-    @ApiResponse({ status: 200, description: 'Lista de clientes obtenida con éxito' })
-    public async getAllClients() {
-        return this._clientService.findAll();
-    }
+  @Get()
+  @ApiOperation({ summary: 'Listar todos los registros de clientes' })
+  @ApiResponse({ status: 200, description: 'Lista de clientes obtenida con éxito' })
+  public async getAllClients() {
+    return this._clientService.findAll();
+  }
 
-    @Post()
-    @ApiOperation({ summary: 'Crear un nuevo cliente' })
-    @ApiResponse({ status: 201, description: 'Cliente creado con éxito' })
-    @ApiBody({
-        description: 'Ejemplo de datos para crear un nuevo cliente',
-        type: CreateClientRequestDto,
-        examples: {
-          ejemplo: {
-            summary: 'Ejemplo de cliente',
-            value: {
-              name: 'John Doe',
-              address: '123 Main St',
-              phone: '+1234567890',
-              createdDate: '2024-10-27T12:34:56.000Z',
-            },
-          },
+  @Post()
+  @ApiOperation({ summary: 'Crear un nuevo cliente' })
+  @ApiResponse({ status: 201, description: 'Cliente creado con éxito' })
+  @ApiBody({
+    description: 'Ejemplo de datos para crear un nuevo cliente',
+    type: CreateClientRequestDto,
+    examples: {
+      ejemplo: {
+        summary: 'Ejemplo de cliente',
+        value: {
+          name: 'John Doe',
+          address: '123 Main St',
+          phone: '+1234567890',
+          createdDate: '2024-10-27T12:34:56.000Z',
         },
-      })
-    public async createClient(
-        @Body() createClientDto: CreateClientRequestDto
-    ) {
-        return this._clientService.create(createClientDto);
-    }
+      },
+    },
+  })
+  public async createClient(
+    @Body() createClientDto: CreateClientRequestDto
+  ) {
+    return this._clientService.create(createClientDto);
+  }
 
-    @Put(':id')
-    @ApiOperation({ summary: 'Modificar un cliente ya existente' })
-    @ApiResponse({ status: 200, description: 'Cliente actualizado con éxito' })
-    public async updateClient(
-        @Param('id') id: string, 
-        @Body() updateClientDto: any
-    ) {
-        return this._clientService.update(id, updateClientDto);
-    }
+  @Put(':id')
+  @ApiOperation({ summary: 'Modificar un cliente ya existente' })
+  @ApiResponse({ status: 200, description: 'Cliente actualizado con éxito' })
+  public async updateClient(
+    @Param('id') id: string,
+    @Body() updateClientDto: any
+  ) {
+    return this._clientService.update(id, updateClientDto);
+  }
 }
